df4569cb68f43815c2c2dd680fecc0366cf0a9a0
GiovanniAndrex <giovanniandrex@gmail.com>
2024-10-15 09:55:49 -0500

    Solicitar Permisos P1

--- lib/presentation/blocs/notifications/notifications_bloc.dart
+++ lib/presentation/blocs/notifications/notifications_bloc.dart
@@ -6,9 +6,32 @@ part 'notifications_event.dart';
 part 'notifications_state.dart';
 
 class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
+
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+
   NotificationsBloc() : super(const NotificationsState()) {
     // on<NotificationsEvent>((event, emit) {
     //   // TODO: implement event handler
     // });
+  }
+
+  void requestPermission() async {
+    NotificationSettings settings = await messaging.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      carPlay: false,
+      criticalAlert: true,
+      provisional: false,
+      sound: true,
+    );
+    if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+      // User granted permission
+    } else if (settings.authorizationStatus ==
+        AuthorizationStatus.provisional) {
+      // User granted provisional permission
+    } else {
+      // User denied permission
+    }
   }
 }
