a3dc0da9e1edcd54c2ee6c7b52986b661817d835
GiovanniAndrex <giovanniandrex@gmail.com>
2024-10-15 09:20:00 -0500

    Init Project - Basic configuration

---
+++ lib/presentation/screens/home_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.settings),
+          )
+        ],
+      ),
+      body: const _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  const _HomeView();
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 10,
+      itemBuilder: (context, index) {
+        return SizedBox(
+          height: 100,
+          child: Center(
+            child: Text('Item $index'),
+          ),
+        );
+      },
+    );
+  }
+}
